2c88fef6d22700b8ef3c5f0f5c7975c4dbcae714
superYang <757097678@qq.com>
2015-09-19 16:37:31 +0800

    项目初始化

---
+++ YCDatePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YCDatePicker
+//
+//  Created by 超杨 on 15/9/19.
+//  Copyright (c) 2015年 超杨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YCDatePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YCDatePicker
+//
+//  Created by 超杨 on 15/9/19.
+//  Copyright (c) 2015年 超杨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
